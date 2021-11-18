c3c6feadd6a1dbc818df5da8f1fc3ddec4f92649
AndyHo <h880702520@gmail.com>
2021-11-18 12:20:17 +0800

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card>
+        <v-app-bar elevation="2">
+            <v-toolbar-title>{{ toolBarTitle }}</v-toolbar-title>
+            <v-switch class="theme_switcher"
+                v-model="$vuetify.theme.dark"
+                label="深色模式"
+                inset
+            ></v-switch>
+        </v-app-bar>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        toolBarTitle: {
+            type: String,
+            require: true
+        }
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.theme_switcher {
+    position: absolute;
+    right: 0;
+    top: 25%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card height="800" elevation="4" rounded="xl" class="my-4">
+        <v-card-title>地圖的標題</v-card-title>
+        <v-card-subtitle>地圖的子標題</v-card-subtitle>
+        <v-card-text>地圖的文字或內容</v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
