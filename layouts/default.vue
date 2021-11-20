3f36d0f1a86712f1b1a444848315638a323afa86
AndyHo <h880702520@gmail.com>
2021-11-20 23:42:06 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,27 @@
 <template>
-  <v-app>
-    <!-- 標頭 -->
-    <Header toolBarTitle="搶救花果山"/>
-    <!-- 頁面內容 -->
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-footer>
-      <!-- 版權聲明 -->
-      <v-col class="text-center">
-        &copy; {{ new Date().getFullYear() }} 健行科技大學 版權所有
-      </v-col>
-    </v-footer>
-  </v-app>
+    <v-app>
+        <!-- 標頭 -->
+        <Header toolBarTitle="搶救花果山" />
+        <!-- 頁面內容 -->
+        <v-main>
+            <v-container>
+                <Nuxt />
+            </v-container>
+        </v-main>
+        <v-footer>
+            <v-col class="text-center">
+                &copy; {{ new Date().getFullYear() }} 健行科技大學 版權所有
+            </v-col>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-    }
-  }
+    data() {
+        return {}
+    },
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
